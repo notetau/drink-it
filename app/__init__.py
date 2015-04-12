8defcdcab154d35a41934a1f536442dc8ef52fd5
notetau <nonotetau@gmail.com>
2015-04-12 11:50:29 +0900

    change twitter login url authorize -> authenticate

--- app/__init__.py
+++ app/__init__.py
@@ -25,7 +25,7 @@ twitter = OAuth1Service(
     name='twitter',
     request_token_url='https://api.twitter.com/oauth/request_token',
     access_token_url='https://api.twitter.com/oauth/access_token',
-    authorize_url='https://api.twitter.com/oauth/authorize',
+    authorize_url='https://api.twitter.com/oauth/authenticate',
     base_url='https://api.twitter.com/1.1/'
 )
 
