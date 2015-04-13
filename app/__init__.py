ea4212c8220afc69dc847fb2b4cfb8bbc108bf02
notetau <nonotetau@gmail.com>
2015-04-13 23:35:28 +0900

    rename UserInfo LoginUser

--- app/__init__.py
+++ app/__init__.py
@@ -32,7 +32,7 @@ twitter = OAuth1Service(
 # データベースの起動
 model.model_init()
 
-class UserInfo:
+class LoginUser:
     @staticmethod
     def is_login():
         return "login" in flask.session
@@ -68,17 +68,16 @@ def before_request():
 @app.route("/")
 def index():
     """ トップページ """
-
-    login_flag = UserInfo.is_login()
+    login_flag = LoginUser.is_login()
     loginout = '''<a href="./login/twitter">
         <img src="./static/img/sign-in-with-twitter-gray.png" alt="sign in with twitter"/>
         </a>'''
     if login_flag:
-        if UserInfo.get_login_type() == model.LOGIN_TYPE_TWITTER:
+        if LoginUser.get_login_type() == model.LOGIN_TYPE_TWITTER:
             loginout = '{0} <a href="./logout">ログアウト</a>'.format(
-                UserInfo.get_user_display_name())
+                LoginUser.get_user_display_name())
         else:
-            app.logger.warning("invalid access")
+            app.logger.warning("invalid login type user_id={0}".format(LoginUser.get_user_id()))
 
     login_flag = str(login_flag).lower() # convert to javascript boolean literal
     return flask.render_template("index.html", loginout=loginout, login_flag=login_flag)
@@ -104,17 +103,16 @@ def login_with_twitter_callback():
     try:
         app.logger.info("#### login_with_twitter_callback ####")
         request_token, request_token_secret = flask.session["twitter_oauth"]
-
-        creds = {'request_token': request_token,
+        requests = {'request_token': request_token,
                 'request_token_secret': request_token_secret}
         params = {'oauth_verifier': flask.request.args['oauth_verifier']}
-        sess = twitter.get_auth_session(params=params, **creds)
+        sess = twitter.get_auth_session(params=params, **requests)
         verify = sess.get("account/verify_credentials.json").json()
         app.logger.info("twitter login success({0},{1})".format(verify["screen_name"], verify["id"]))
         new_user = model.User(name = str(verify["id"]), login_type = model.LOGIN_TYPE_TWITTER)
         user_id = model.add_user(new_user)
         # ログイン状態をセッションに保存
-        UserInfo.login(user_id, new_user.login_type, verify["screen_name"])
+        LoginUser.login(user_id, new_user.login_type, verify["screen_name"])
     except:
         app.logger.info("twitter login fail")
 
@@ -124,7 +122,7 @@ def login_with_twitter_callback():
 @app.route('/logout')
 def logout():
     """ログアウト"""
-    UserInfo.logout() # ログイン状態を解除
+    LoginUser.logout() # ログイン状態を解除
     app.logger.info('You were signed out')
     return flask.redirect(flask.request.referrer or flask.url_for('index'))
 
@@ -132,11 +130,11 @@ def logout():
 @app.route("/api/add_new_drink", methods=["POST"])
 def api_add_new_drink():
     """ 管理する飲み物を追加する"""
-    if UserInfo.is_login():
+    if LoginUser.is_login():
         if "drink_name" not in flask.request.form:
             return "invalid data", 400
         drink_name = urllib.parse.unquote(flask.request.form['drink_name'])
-        data_dict = model.append_drink_list(UserInfo.get_user_id(), drink_name)
+        data_dict = model.append_drink_list(LoginUser.get_user_id(), drink_name)
         data_dict["name"] = drink_name
         return flask.jsonify(data_dict), 200
     else:
@@ -149,9 +147,9 @@ def api_put_drink(drink_id):
     print("put_drink")
     count = int(flask.request.form['update_count'])
 
-    if UserInfo.is_login():
-        print(UserInfo.get_user_id(), drink_id, count)
-        if model.add_drink_history(UserInfo.get_user_id(), drink_id, count):
+    if LoginUser.is_login():
+        print(LoginUser.get_user_id(), drink_id, count)
+        if model.add_drink_history(LoginUser.get_user_id(), drink_id, count):
             return "OK", 200
         else:
             return "internal error", 500
@@ -162,8 +160,8 @@ def api_put_drink(drink_id):
 @app.route("/api/<drink_id>/stat", methods=["GET"])
 def api_get_drink_stat(drink_id):
     """飲んだ履歴の取得"""
-    if UserInfo.is_login():
-        data = model.get_drink_history_stat(UserInfo.get_user_id(), drink_id)
+    if LoginUser.is_login():
+        data = model.get_drink_history_stat(LoginUser.get_user_id(), drink_id)
         return data, 200
     else:
         return "not login", 403
@@ -172,8 +170,8 @@ def api_get_drink_stat(drink_id):
 @app.route("/api/all_drink_list", methods=["GET"])
 def api_get_all_drink_list():
     """飲み物一覧を取得"""
-    if UserInfo.is_login():
-        all_drink = model.get_drink_list_by_json(UserInfo.get_user_id())
+    if LoginUser.is_login():
+        all_drink = model.get_drink_list_by_json(LoginUser.get_user_id())
         return all_drink, 200
     else:
         return "not login", 403
